dd0a031feb3a664b9903d44dbc9d385d49059e24
Gadget Store <kirezigustave22@gmail.com>
2026-05-26 09:47:01 +0200

    my frst commit on first project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+   
+  <h1 class="font-bold text-center text-blue-500">SOS Technical School</h1>
+  <p>
+Empowering future professionals through quality technical education,
+practical skills training, innovation, and excellence to build a better
+world with technology and digital solutions.
+  </p>
+</template>
+
+
+    
