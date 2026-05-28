0798d0a07d8fb279e9268f3ad268dcd950c399e2
Gadget Store <kirezigustave22@gmail.com>
2026-05-28 23:16:18 +0200

    first commit

--- src/view/About.vue
+++ src/view/About.vue
@@ -1,9 +1,22 @@
 <template>
-    <section class="bg-white text-green-600 h-screen w-full mt-12">
+    <!--About Us-->
 
-        <h1 class="text-5xl font-bold py-24 text-center">
-            welcome to Rwanda
-        </h1>
+    <section class=" relation h-screen bg-screen bg-cover bg-center flex flex-col items-center justify-center text-white "
+    style="background-image:url('../src/assets/city.png')">
+    <div class="bg-black/40 bg-opacity-50 absolute inset-0">
+
+    </div>
+    <div class="relative text-center px-4">
+        <h1 class="text-5xl font-bond mb-4">CLEANLINESS OF THE CITY</h1>
+        <p class="text-xl mb text-green-300">The country provide you a better view for it Nature
+            and climate with cleaness and security to all.
+        </p>
+
+    </div>
+    <button class="text-white bg-green-500 text-center px-6 py-3">
+        click here
+    </button>
 
     </section>
-</template>
+</template> 
+
--- src/view/home.vue
+++ src/view/home.vue
@@ -2,8 +2,8 @@
 <template>
     <!--Hero section-->
 
-    <section class=" relation h-screen bg-screen bg-cover bg-center flex item-center justify-center text-white "
-    style="background-image:url('../src/assets/vw.png')">
+    <section class=" relation h-screen bg-screen bg-cover bg-center flex flex-col items-center justify-center text-white "
+    style="background-image:url('../src/assets/city.png')">
     <div class="bg-black/40 bg-opaity-50 absolute inset-0">
 
     </div>
@@ -12,5 +12,11 @@
         <p class="text-xl mb text-green-300">the land  of a thousand of hills</p>
 
     </div>
+
+    <button class="text-white bg-green-500 text-center px-6 py-3">
+        click here
+    </button>
     </section>
 </template>
+
+
